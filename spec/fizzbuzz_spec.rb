48ff94c6f29c6544b53487f12975cae94a1e32af
YoyoCodes <laura.d.petre@gmail.com>
2018-05-10 11:45:37 +0100

    wrote test for indivisible number; added code for that

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,4 +11,9 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 75' do
     expect(fizzbuzz(75)).to eq 'fizzbuzz'
   end
+
+  it 'returns 49 when passed 49' do
+    expect(fizzbuzz(49)).to eq 49
+  end
+ 
 end
